2c930058e2d19e376c75862825dd61ba5b653bc0
guillaume iter <guillaume.saintetienne@iter.org>
2024-07-03 15:28:50 +0200

    find a way to push json-serialized data object  to http resposne

--- src/main/kotlin/driver/AccountingHttpServer.kt
+++ src/main/kotlin/driver/AccountingHttpServer.kt
@@ -4,9 +4,15 @@ package driver
 import org.http4k.core.*
 import org.http4k.core.Status.Companion.OK
 import org.http4k.filter.ServerFilters.CatchLensFailure
+import org.http4k.lens.Query
+import org.http4k.lens.string
 import org.http4k.routing.bind
-import org.http4k.routing.path
 import org.http4k.routing.routes
+import org.http4k.core.Body
+import org.http4k.format.Jackson.auto
+
+//import org.http4k.format.Moshi
+// import org.http4k.routing.path
 import org.http4k.server.Http4kServer
 import org.http4k.server.Jetty
 import org.http4k.server.asServer
@@ -20,11 +26,18 @@ fun accountingHttpServer(port: Int, useCaseReadBalance: UseCaseReadBalance): Htt
 
 fun accountingHttpHandler(useCaseReadBalance: UseCaseReadBalance): HttpHandler = CatchLensFailure.then(
     routes(
-        "/balance/{accountId}" bind Method.GET to { request: Request ->
-            val accountId = request.path("accountId")!!
+        "/balance" bind Method.GET to { request: Request ->
+             val accountIdRequest = Query.string().required("accountId")
+            val accountId = accountIdRequest(request)
             val balance = useCaseReadBalance.readBalance()
+
+
             val dtoBalance = DTOBalance(balance.account(accountId).value())
-            Response(OK).body("here we shoul")
+            val bodyJson = Body.auto<DTOBalance>().toLens()
+            dtoBalance.let {
+                Response(OK).with( bodyJson of it)
+            }
+
         }
 
     )
